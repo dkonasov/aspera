3c2d788940dcf464106b51d35c2bc4360f3be71f
Konasov Dmitriy <d.konasov@yandex.ru>
2023-08-29 22:07:52 +0300

    refactored global styles for tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,8 +5,15 @@ module.exports = {
 		extend: {
 			colors: {
 				'accent': '#2337ff',
+				'black': '#0f1219',
+				'gray': '#60739f',
+				'grayLight': '#e5e9f0',
+				'grayDark': '#222939'
+			},
+			boxShadow: {
+				card: '0 2px 6px rgba(96, 115, 159, 25%), 0 8px 24px rgba(96, 115, 159, 33%), 0 16px 32px rgba(96, 115, 159, 33%)'
 			}
-		}
+		},
 	},
 	plugins: [],
 }
