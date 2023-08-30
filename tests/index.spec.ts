d9a9074ffdaeea3453ba3cc8c9647d623faeeb5c
Konasov Dmitriy <d.konasov@yandex.ru>
2023-08-31 01:11:25 +0300

    plawright integration and first test

---
+++ tests/index.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from "@playwright/test";
+
+test("has title", async ({ page }) => {
+  await page.goto("http://localhost:8080/");
+
+  await expect(page).toHaveTitle("Dmitrii Konasov");
+});
